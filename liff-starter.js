896bd22f4cc9111fe7eb7de07fd78b2f24b21927
YA!福茂 <yaffmo@gmail.com>
2020-10-22 09:49:42 +0800

    fa

--- liff-starter.js
+++ liff-starter.js
@@ -13,70 +13,70 @@ window.onload = function () {
       ])
       .then(liff.closeWindow());
   }
+
+  const useNodeJS = false; // if you are not using a node server, set this value to false
+  const defaultLiffId = "1654926321-xwKZv1pe"; // change the default LIFF value if you are not using a node server
+
+  // DO NOT CHANGE THIS
+  let myLiffId = "";
+
+  // //   // if node is used, fetch the environment variable and pass it to the LIFF method
+  // //   // otherwise, pass defaultLiffId
+  // //   if (useNodeJS) {
+  // //     fetch("/send-id")
+  // //       .then(function (reqResponse) {
+  // //         return reqResponse.json();
+  // //       })
+  // //       .then(function (jsonResponse) {
+  // //         myLiffId = jsonResponse.id;
+  // //         initializeLiffOrDie(myLiffId);
+  // //       })
+  // //       .catch(function (error) {
+  // //         document.getElementById("liffAppContent").classList.add("hidden");
+  // //         document
+  // //           .getElementById("nodeLiffIdErrorMessage")
+  // //           .classList.remove("hidden");
+  // //       });
+  // //   } else {
+  // //     myLiffId = defaultLiffId;
+  // //     initializeLiffOrDie(myLiffId);
+  // //   }
+  // // };
+
+  // // /**
+  // //  * Check if myLiffId is null. If null do not initiate liff.
+  // //  * @param {string} myLiffId The LIFF ID of the selected element
+  // //  */
+  // // function initializeLiffOrDie(myLiffId) {
+  // //   if (!myLiffId) {
+  // //     document.getElementById("liffAppContent").classList.add("hidden");
+  // //     document.getElementById("liffIdErrorMessage").classList.remove("hidden");
+  // //   } else {
+  // //     initializeLiff(myLiffId);
+  // //   }
+  // // }
+
+  // /**
+  //  * Initialize LIFF
+  //  * @param {string} myLiffId The LIFF ID of the selected element
+  //  */
+  function initializeLiff(myLiffId) {
+    liff
+      .init({
+        liffId: myLiffId,
+      })
+      .then(() => {
+        // start to use LIFF's api
+        initializeApp();
+      });
+    // .catch((err) => {
+    //   document.getElementById("liffAppContent").classList.add("hidden");
+    //   document
+    //     .getElementById("liffInitErrorMessage")
+    //     .classList.remove("hidden");
+    // });
+  }
 };
-// //   const useNodeJS = false; // if you are not using a node server, set this value to false
-// //   const defaultLiffId = "1654926321-xwKZv1pe"; // change the default LIFF value if you are not using a node server
-
-// //   // DO NOT CHANGE THIS
-// //   let myLiffId = "";
-
-// //   // if node is used, fetch the environment variable and pass it to the LIFF method
-// //   // otherwise, pass defaultLiffId
-// //   if (useNodeJS) {
-// //     fetch("/send-id")
-// //       .then(function (reqResponse) {
-// //         return reqResponse.json();
-// //       })
-// //       .then(function (jsonResponse) {
-// //         myLiffId = jsonResponse.id;
-// //         initializeLiffOrDie(myLiffId);
-// //       })
-// //       .catch(function (error) {
-// //         document.getElementById("liffAppContent").classList.add("hidden");
-// //         document
-// //           .getElementById("nodeLiffIdErrorMessage")
-// //           .classList.remove("hidden");
-// //       });
-// //   } else {
-// //     myLiffId = defaultLiffId;
-// //     initializeLiffOrDie(myLiffId);
-// //   }
-// // };
-
-// // /**
-// //  * Check if myLiffId is null. If null do not initiate liff.
-// //  * @param {string} myLiffId The LIFF ID of the selected element
-// //  */
-// // function initializeLiffOrDie(myLiffId) {
-// //   if (!myLiffId) {
-// //     document.getElementById("liffAppContent").classList.add("hidden");
-// //     document.getElementById("liffIdErrorMessage").classList.remove("hidden");
-// //   } else {
-// //     initializeLiff(myLiffId);
-// //   }
-// // }
-
-// // /**
-// //  * Initialize LIFF
-// //  * @param {string} myLiffId The LIFF ID of the selected element
-// //  */
-// // function initializeLiff(myLiffId) {
-// //   liff
-// //     .init({
-// //       liffId: myLiffId,
-// //     })
-// //     .then(() => {
-// //       // start to use LIFF's api
-// //       initializeApp();
-// //     })
-// //     .catch((err) => {
-// //       document.getElementById("liffAppContent").classList.add("hidden");
-// //       document
-// //         .getElementById("liffInitErrorMessage")
-// //         .classList.remove("hidden");
-// //     });
-// // }
-
 // // /**
 // //  * Initialize the app by calling functions handling individual app components
 // //  */
