cbc8040f7699771699dedb0f1d0fad6b75ad7a9f
mo_ya <mo_ya@MSI>
2021-01-26 11:10:57 +0800

    v4

--- liff-starter.js
+++ liff-starter.js
@@ -221,7 +221,10 @@ function registerButtonHandlers() {
             profile.displayName
 
           url.searchParams.append('ssn30', profile.userId)
-
+          liff.openWindow({
+            url: url,
+            external: true,
+          })
           // window.location.replace(url)
           const profilePictureDiv = document.getElementById('profilePictureDiv')
           if (profilePictureDiv.firstElementChild) {
@@ -235,12 +238,6 @@ function registerButtonHandlers() {
           document.getElementById('statusMessageField').textContent =
             profile.statusMessage
           toggleProfileData()
-        })
-        .then(() => {
-          liff.openWindow({
-            url: url,
-            external: true,
-          })
         })
         .catch(function (error) {
           window.alert('Error getting profile: ' + error)
